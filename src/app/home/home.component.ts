5c419171f2ed517a2ded83447f3302c2887d1e86
zubair20 <zubair.akhtar20@gmail.com>
2023-05-07 18:54:50 +0500

    change text

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -80,7 +80,7 @@ export class HomeComponent {
       if(e.target.value){
         this.neonText = e.target.value;
       }else{
-        this.neonText = "Your Text";
+        this.neonText = "My Text";
       }
       
     } catch(e) {
